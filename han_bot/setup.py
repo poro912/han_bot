9fec33335f9735c5a60d087a8902cc1f482771f5
201807042 <angus99@naver.com>
2021-08-02 06:51:25 +0900

    han project first commit

---
+++ han_bot/setup.py
@@ -0,0 +1,42 @@
+
+
+import discord
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("**--------------------------------------**")
+    # print(client.user.id)
+    # print("버전" + han.ver)
+    # print("이름 : " + client.user.name)
+    print("**--------------------------------------**")
+    # await client.change_presence(status=discord.Status.online , activity=discord.Game("'한이야 도움말' 입력! / 제작 이음, 포로"))
+    # prt_command()
+
+
+
+
+print("function : ",__name__);
+print(" : ",__build_class__);
+print("파일 위치 : ",__file__);
+
+
+
+
+
+
+
+
+
+
+# @client.event
+"""
+try:
+
+except:
+"""
+
+
+
