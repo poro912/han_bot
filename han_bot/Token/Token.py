1708c2350544f839e25f2b23baa8bc513dba3691
201807042 <angus99@naver.com>
2021-08-16 23:37:18 +0900

    파일시스템구축

---
+++ han_bot/Token/Token.py
@@ -0,0 +1,26 @@
+import os
+
+
+def set_token():
+    global han_token
+    global test_token
+    file_name = "code.txt"
+    
+    before = os.getcwd()
+    os.chdir("../../")
+    now = os.getcwd()
+    file = open(now+"/"+file_name,"r")
+    
+    test_token = file.readline()
+    han_token = file.readline()
+    
+    print("토큰 가져오기 완료")
+    os.chdir(before)
+
+def ret_token(type):
+    global test_token
+    global han_token
+    if(type == 1 or type == "origin" or type == "1"):
+        return test_token
+    if(type == 2 or type == "test" or type == "2"):
+        return han_token
--- han_bot/setup.py
+++ han_bot/setup.py
@@ -1,33 +1,54 @@
 #git hub test
-
+import os
+import sys
 import discord
+import asyncio
+
+#파일 시스템 구축
+files = ["Excl", "Option", "Admin", "Token"]
+path = os.path.dirname(os.path.realpath(__file__))
+path.replace("\\","/")
+#파일 시스템 등록
+for i in files:
+    sys.path.append(path+"\\"+i)
+    print(path+"\\"+i)
+
+#파일 시스템 임포트
+#from Excl import *
+#from Option import *
+#from Admin import *
+from Token import *
 
 
+global msg;
+
+token_type = "origin"
 client = discord.Client()
 
 @client.event
 async def on_ready():
+    print(os.path.dirname(os.path.realpath(__file__))+"\\Excl")
     print("**--------------------------------------**")
-    # print(client.user.id)
-    # print("버전" + han.ver)
-    # print("이름 : " + client.user.name)
+    print(client.user.id)
+    #print("버전" + han.ver)
+    #print("이름 : " + client.user.name)
     print("**--------------------------------------**")
-    # await client.change_presence(status=discord.Status.online , activity=discord.Game("'한이야 도움말' 입력! / 제작 이음, 포로"))
-    # prt_command()
+    await client.change_presence(status=discord.Status.online , activity=discord.Game("'한이야 도움말' 입력! / 제작 이음, 포로"))
 
+@client.event
+async def on_message(message):
+    msg = message
+    if message.content.startswith("한이") :
+        ''' 한이 주요 코드부'''
+        # await message.channel.send("우씨... 나 말 안해!😡");
+        #message_submit(message);
+        print("실행")
+        print(" : ",__build_class__)
+        print("파일 위치 : ",__file__)
 
-
-
-print("function : ",__name__);
-print(" : ",__build_class__);
-print("파일 위치 : ",__file__);
-
-
-
-
-
-
-
+Token.set_token()
+global test_token
+client.run(Token.ret_token(token_type))
 
 
 
@@ -38,5 +59,6 @@ try:
 except:
 """
 
+# token.get_token();
 
 
